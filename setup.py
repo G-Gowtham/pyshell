d83c3fab27dcf1dc1113ae8f0b399b570251cc1c
G-Gowtham <gowthamg@presidio.com>
2021-03-26 12:59:11 +0530

    added folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='pyshell',
+version='0.1',
+description='Python implementation of unix shell',
+url='https://github.com/G-Gowtham/python-unix-shell',
+author='gowtham',
+author_email='ggowtham610@gmail.com',
+packages=['pyshell'],
+install_requires=['termcolor']
+)
--- shell-init.sh
+++
@@ -1,5 +0,0 @@
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-GREEN='\033[0;32m'
-NC='\033[0m'
-export PS1="${BLUE}$USER@$HOSTNAME(i) : $(pwd)\$ ${NC}"
